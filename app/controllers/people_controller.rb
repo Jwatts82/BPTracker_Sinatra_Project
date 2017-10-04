4dd05181d5b93801cae9c98f99ee37ac2fcb7534
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-04 03:50:04 -0500

    WIP; #emtpy_input? checks for empty fields, if method returns true user sent back to 'people/new' with a flash message.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,6 +3,24 @@ class PeopleController < ApplicationController
     logged_in? ? (erb :'/people/new') : (redirect '/')
   end
 
+  post '/people' do
+
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @dob = params[:dob]
+
+    person = Person.new
+    if person.emtpy_input?(params)
+       flash[:message] = 'Some required information is missing or incomplete.' \
+                         ' Please correct your entries and try again.'
+       erb :'people/new'
+    else
+      # create new person object
+      
+      redirect "/people/#{person.id}"
+    end
+  end
+
   get '/people/:id' do
     logged_in? ? (erb :'/people/show') : (redirect '/')
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,7 @@ class Person < ActiveRecord::Base
   has_many :readings, dependent: :destroy
   has_many :comments, through: :readings
 
+  def emtpy_input?(hash)
+    hash.values.any? {|v| v.empty? }
+  end
 end
