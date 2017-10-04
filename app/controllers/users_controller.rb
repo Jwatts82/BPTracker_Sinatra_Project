cfe20c600e6a1db6ac533ba01f67d476a2487a26
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-03 22:25:55 -0500

    WIP; User will be redirected to the signup page where a flash message will appear if all fields are not filled in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :"/users/signup"
   end
+
+  post '/signup' do
+
+    if params[:username].empty? || params[:password].empty?
+      flash[:message] = 'Some required information is missing or incomplete.' \
+                        ' Please correct your entries and try again.'
+
+      erb :"/users/signup"
+    else
+      # create User obj
+    end
+  end
 end
