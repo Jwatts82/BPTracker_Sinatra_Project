3dfafb67886225620927659efcf28b108063126a
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-04 04:57:34 -0500

    WIP; Set up '/readings/new' route and form to allow a user to add a bp reading with a comment.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -3,4 +3,8 @@ class ReadingsController < ApplicationController
   get '/readings' do
     erb :'readings/index'
   end
+
+  get '/readings/new' do
+    logged_in? ? (erb :'/readings/new') : (redirect '/')
+  end
 end
