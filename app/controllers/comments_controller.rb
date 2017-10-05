9018f6a473de98581fe91f22e3cb4d6766d1addf
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-04 20:28:11 -0500

    WIP; Created route to view all readings comments from the current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,13 @@
 class CommentsController < ApplicationController
 
+  get '/comments' do
+    if logged_in?
+      user = User.find(session[:u_id])
+      @person = Person.find(user.person_id)
+
+      erb :"/comments/index"
+    else
+      redirect '/'
+    end
+  end
 end
