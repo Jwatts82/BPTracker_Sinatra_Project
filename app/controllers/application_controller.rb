70f8bc25132753dd6b0161ddafa9ee125609aadc
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-14 11:59:24 -0500

    Changed #logged_in to use current_user. #current_user uses #find_by to give a more explicit error message & session will hold a key called 'user_id'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,11 +19,11 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def logged_in?
-      !!session[:u_id]
+      !!current_user
     end
 
     def current_user
-      User.find(session[:u_id])
+      User.find_by(id: session[:user_id])
     end
   end
 
