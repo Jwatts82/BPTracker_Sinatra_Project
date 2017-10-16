0ae36c4699e7e600f520586504d2574e9375c55a
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-16 01:19:01 -0500

    WIP; Deleted Person model, added reading and comment association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
-  belongs_to :person
+  include InputCheck::InstanceMethods
+
+  has_many :readings, dependent: :destroy
+  has_many :comments, through: :readings
 
   has_secure_password
   validates :username, presence: true, uniqueness: true
+
+  def age_calculator(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month ||
+      (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 end
