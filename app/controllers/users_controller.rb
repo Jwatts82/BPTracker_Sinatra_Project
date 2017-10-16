9504b7dc3a9c1cbe65d0f6f29132161e45bbab6d
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-16 01:57:38 -0500

    WIP; Completed POST '/user' route. Made '/user/:id' route.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     user = User.new(params[:user])
     if user.save
       session[:user_id] = user.id
-      redirect '/people/new'
+      redirect '/user/new'
     else
       flash[:message] = user.errors.full_messages.join(', ')
       erb :'/users/signup'
@@ -41,6 +41,27 @@ class UsersController < ApplicationController
   end
 
   post '/user' do
+    user = current_user
+
+    if !user.emtpy_input?(params[:user])
+      user.update(params[:user])
+      user.age = user.age_calculator(params[:user][:dob].to_date)
+      user.save
+
+      redirect "/user/#{user.id}"
+    else
+      flash[:message] = 'Some required information is missing or incomplete.' \
+        ' Please correct your entries and try again.'
+
+      @first_name = params[:user][:first_name]
+      @last_name = params[:user][:last_name]
+      @dob = params[:user][:dob]
+
+      erb :'/users/new'
+    end
+  end
+
+  get '/user/:id' do
     binding.pry
   end
 
