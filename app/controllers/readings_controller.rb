49c4ea73d00a5d1013802cfa0d583f04686f79fb
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-16 03:10:00 -0500

    WIP; The new view has more standard HTML formats and uses #current_user to get access to to the user object

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -17,43 +17,8 @@ class ReadingsController < ApplicationController
   end
 
   post "/readings" do
-    user = User.find(session[:u_id])
-
-    @date = params[:date]
-    @time = params[:time]
-    @systolic = params[:systolic]
-    @diastolic = params[:diastolic]
-    @pulse = params[:pulse]
-    @comment = params[:comment]
-
-    reading = Reading.new
-
-    v = params.select {|k,v| v unless k == 'content' }
-    if !reading.emtpy_input?(v) &&
-       reading.category_selector(params[:systolic], params[:diastolic])
-      reading.systolic = params[:systolic]
-      reading.diastolic = params[:diastolic]
-      reading.pulse = params[:pulse]
-      reading.person_id = user.person_id
-
-      datetime = reading.datetime_sql_insert(params[:date], params[:time])
-      reading.reading_date_time = datetime
-
-      reading.category = reading.category_selector(params[:systolic],
-                                                  params[:diastolic])
-
-      params[:content].empty? ? comment = Comment.create(content: 'no comment') :
-                            comment = Comment.create(content: params[:content])
-      reading.comments << comment
-      reading.save
-
-      redirect "/readings/#{reading.id}"
-    else
-      flash[:message] = 'Some required information is missing or your BP ' \
-                        'reading is not possible. Please review your input.'
-
-       erb :'/readings/new'
-    end
+    binding.pry
+    
   end
 
   post '/readings/selection' do
